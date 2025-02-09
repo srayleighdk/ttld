79b49e199fff491001149cedf5b7ec478ad2f725
DK <srayleighdk@gmail.com>
2025-02-09 18:58:35 +0700

    theme provider

---
+++ lib/themes/colors/color_style.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:ttld/themes/colors/base_color_style.dart';
+
+abstract class ColorStyles extends BaseColorStyles {
+  /// Available styles
+
+  // general
+  @override
+  Color get background;
+  @override
+  Color get content;
+  @override
+  Color get primaryAccent;
+
+  @override
+  Color get surfaceBackground;
+  @override
+  Color get surfaceContent;
+
+  // app bar
+  @override
+  Color get appBarBackground;
+  @override
+  Color get appBarPrimaryContent;
+
+  @override
+  Color get buttonBackground;
+  @override
+  Color get buttonContent;
+
+  @override
+  Color get buttonSecondaryBackground;
+  @override
+  Color get buttonSecondaryContent;
+
+  // bottom tab bar
+  @override
+  Color get bottomTabBarBackground;
+
+  // bottom tab bar - icons
+  @override
+  Color get bottomTabBarIconSelected;
+  @override
+  Color get bottomTabBarIconUnselected;
+
+  // bottom tab bar - label
+  @override
+  Color get bottomTabBarLabelUnselected;
+  @override
+  Color get bottomTabBarLabelSelected;
+
+  // toast notification
+  Color get toastNotificationBackground;
+
+  // e.g. add a new style
+  // Uncomment the below:
+  // Color get iconBackground;
+
+  // Then implement in color in:
+  // /resources/themes/styles/light_theme_colors
+  // /resources/themes/styles/dark_theme_colors
+}
