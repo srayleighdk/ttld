75ce24524a8d4cc0a74a6256241df7a122de3c3e
DK <srayleighdk@gmail.com>
2025-02-20 08:05:11 +0700

    feat: Replace PaginatedDataTable2 with PaginatedDataTable

--- lib/pages/hosoungvien/hosoungvien_page.dart
+++ lib/pages/hosoungvien/hosoungvien_page.dart
@@ -18,79 +18,80 @@ class HoSoUngVienPage extends StatelessWidget {
                 HoSoUngVienApiService(locator<ApiClient>().dio))
           ..add(HoSoUngVienFetchData(page: 1)),
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Hồ Sơ Ứng Viên'),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    // Navigate to create new HoSoUngVien
-                  },
-                ),
-              ],
-            ),
-            body: BlocBuilder<HoSoUngVienBloc, HoSoUngVienState>(
-              builder: (context, state) {
-                if (state is HoSoUngVienInitial) {
-                  return const Center(child: Text('Initial State'));
-                } else if (state is HoSoUngVienLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state is HoSoUngVienLoaded) {
-                  return PaginatedDataTable2(
-                    columns: const [
-                      DataColumn2(label: Text('Họ tên'), fixedWidth: 150),
-                      DataColumn2(label: Text('Email'), size: ColumnSize.M),
-                      DataColumn2(label: Text('Hành động')),
-                    ],
-                    source: HoSoUngVienDataSource(
-                      context: context,
-                      hoSoUngVienList: state.hoSoUngVienList,
-                    ),
-                    empty: Center(child: Text("Không có dữ liệu")),
-                  );
-                } else if (state is HoSoUngVienError) {
-                  return Center(child: Text('Error: ${state.message}'));
-                } else {
-                  return const Center(child: Text('Unknown state'));
-                }
-              },
-            ),
-            bottomNavigationBar: BlocBuilder<HoSoUngVienBloc, HoSoUngVienState>(
-              builder: (context, state) {
-                if (state is HoSoUngVienLoading) {
-                  return const SizedBox.shrink();
-                }
-                int currentPage = 1;
-                if (state is HoSoUngVienLoaded) {
-                  currentPage = state.page;
-                }
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: currentPage > 1
-                          ? () {
-                              context.read<HoSoUngVienBloc>().add(
-                                  HoSoUngVienFetchData(page: currentPage - 1));
-                            }
-                          : null,
-                    ),
-                    Text('Trang $currentPage'),
-                    IconButton(
-                      icon: const Icon(Icons.arrow_forward),
-                      onPressed: state is HoSoUngVienLoaded &&
-                              state.hoSoUngVienList.length == 10
-                          ? () {
-                              context.read<HoSoUngVienBloc>().add(
-                                  HoSoUngVienFetchData(page: currentPage + 1));
-                            }
-                          : null,
-                    ),
+          appBar: AppBar(
+            title: const Text('Hồ Sơ Ứng Viên'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  // Navigate to create new HoSoUngVien
+                },
+              ),
+            ],
+          ),
+          body: BlocBuilder<HoSoUngVienBloc, HoSoUngVienState>(
+            builder: (context, state) {
+              if (state is HoSoUngVienInitial) {
+                return const Center(child: Text('Initial State'));
+              } else if (state is HoSoUngVienLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is HoSoUngVienLoaded) {
+                return PaginatedDataTable(
+                  columns: const [
+                    DataColumn2(label: Text('Họ tên'), fixedWidth: 150),
+                    DataColumn2(label: Text('Email'), size: ColumnSize.M),
+                    DataColumn2(label: Text('Hành động')),
                   ],
+                  source: HoSoUngVienDataSource(
+                    context: context,
+                    hoSoUngVienList: state.hoSoUngVienList,
+                  ),
+                  // empty: Center(child: Text("Không có dữ liệu")),
                 );
-              },
-            )));
+              } else if (state is HoSoUngVienError) {
+                return Center(child: Text('Error: ${state.message}'));
+              } else {
+                return const Center(child: Text('Unknown state'));
+              }
+            },
+          ),
+          // bottomNavigationBar: BlocBuilder<HoSoUngVienBloc, HoSoUngVienState>(
+          //   builder: (context, state) {
+          //     if (state is HoSoUngVienLoading) {
+          //       return const SizedBox.shrink();
+          //     }
+          //     int currentPage = 1;
+          //     if (state is HoSoUngVienLoaded) {
+          //       currentPage = state.page;
+          //     }
+          //     return Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //       children: [
+          //         IconButton(
+          //           icon: const Icon(Icons.arrow_back),
+          //           onPressed: currentPage > 1
+          //               ? () {
+          //                   context.read<HoSoUngVienBloc>().add(
+          //                       HoSoUngVienFetchData(page: currentPage - 1));
+          //                 }
+          //               : null,
+          //         ),
+          //         Text('Trang $currentPage'),
+          //         IconButton(
+          //           icon: const Icon(Icons.arrow_forward),
+          //           onPressed: state is HoSoUngVienLoaded &&
+          //                   state.hoSoUngVienList.length == 10
+          //               ? () {
+          //                   context.read<HoSoUngVienBloc>().add(
+          //                       HoSoUngVienFetchData(page: currentPage + 1));
+          //                 }
+          //               : null,
+          //         ),
+          //       ],
+          //     );
+          //   },
+          // )));
+        ));
   }
 }
 
@@ -123,7 +124,8 @@ class HoSoUngVienDataSource extends DataTableSource {
                 builder: (BuildContext context) {
                   return AlertDialog(
                     title: const Text('Xác nhận xóa'),
-                    content: const Text(('Bạn có chắc chắn muốn xóa hồ sơ này?')),
+                    content:
+                        const Text(('Bạn có chắc chắn muốn xóa hồ sơ này?')),
                     actions: [
                       TextButton(
                         onPressed: () {
