4f6e093d9e2822c8ee21841d2dac79bde8cabafd
DK <srayleighdk@gmail.com>
2025-03-17 09:07:33 +0700

    feat: add recruitment management page for employer

---
+++ lib/pages/home/ntd/quan_ly_tuyen_dung/quan_ly_tuyen_dung.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:ttld/models/tblNhaTuyenDung/tblNhaTuyenDung_model.dart';
+
+class QuanLyTuyenDungPage extends StatefulWidget {
+  final Ntd? ntd;
+  QuanLyTuyenDungPage({super.key, this.ntd});
+
+  @override
+  State<QuanLyTuyenDungPage> createState() => _QuanLyTuyenDungPageState();
+}
+
+class _QuanLyTuyenDungPageState extends State<QuanLyTuyenDungPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
