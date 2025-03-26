c7d8e8a3c29aa2bb8ab26662aa4f083a1e993f71
DK <srayleighdk@gmail.com>
2025-03-26 08:26:17 +0700

    feat: add userName parameter to LoadUserRoles event

--- lib/blocs/user_role_bloc/user_role_event.dart
+++ lib/blocs/user_role_bloc/user_role_event.dart
@@ -2,6 +2,7 @@ part of 'user_role_bloc.dart';
 
 @freezed
 class UserRoleEvent with _$UserRoleEvent {
-  const factory UserRoleEvent.loadRoles() = LoadUserRoles;
-  const factory UserRoleEvent.updateRoles(List<PermissionRole> roles) = UpdateUserRoles;
+  const factory UserRoleEvent.loadRoles(String userName) = LoadUserRoles;
+  const factory UserRoleEvent.updateRoles(List<PermissionRole> roles) =
+      UpdateUserRoles;
 }
