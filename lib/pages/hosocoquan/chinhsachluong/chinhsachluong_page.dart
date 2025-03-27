263afec2611ef3b92a41beb3d10f8168158039d9
DK <srayleighdk@gmail.com>
2025-03-27 21:29:23 +0700

    feat: add chinhsachluong page implementation

---
+++ lib/pages/hosocoquan/chinhsachluong/chinhsachluong_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChinhsachluongPage extends StatefulWidget {
+  const ChinhsachluongPage({super.key});
+
+  @override
+  State<ChinhsachluongPage> createState() => _ChinhsachluongPageState();
+}
+
+class _ChinhsachluongPageState extends State<ChinhsachluongPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
