a1a149fd84621e3580891ecd69a15a6113f4f3db
DK <srayleighdk@gmail.com>
2025-03-28 08:23:10 +0700

    feat: Add phieudknld_m01pli page implementation

---
+++ lib/pages/hosocoquan/phieudknld/phieudknld_m01pli.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PhieudknldM01pli extends StatefulWidget {
+  const PhieudknldM01pli({super.key});
+
+  @override
+  State<PhieudknldM01pli> createState() => _PhieudknldM01pliState();
+}
+
+class _PhieudknldM01pliState extends State<PhieudknldM01pli> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
